8bbb63ab4cf8f9a4094424433babc44bc2686f3c
SafaaIs <SafaaIs@mashreq.com>
2025-05-18 16:55:38 +0300

    commit new change

--- tests/Pages/tests.spec.js
+++ tests/Pages/tests.spec.js
@@ -9,7 +9,7 @@ test('User can sign up successfully', async ({ page }) => {
 
   await page.goto('https://www.demoblaze.com/');
   await signUpPage.openSignUp();
-  await signUpPage.signUp('safaa998', '19949990');
+  await signUpPage.signUp('safaa988', '19949988');
   await signUpPage.assertSignUpSuccess(); 
 });
 
@@ -19,6 +19,6 @@ test('User can log in and log out successfully', async ({ page }) => {
 
   await page.goto('https://www.demoblaze.com/');
   await loginPage.openLogin();
-  await loginPage.login('safaa998', '19949990');
+  await loginPage.login('safaa988', '19949988');
   await loginPage.logout();
 });
