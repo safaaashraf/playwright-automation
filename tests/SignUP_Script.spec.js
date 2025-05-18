24acb59d07e06870cd7b7114482304d2267cc720
SafaaIs <SafaaIs@mashreq.com>
2025-05-18 15:33:45 +0300

    Initial commit - add Playwright tests

---
+++ tests/SignUP_Script.spec.js
@@ -0,0 +1,12 @@
+import{test, expect} from '@playwright/test'
+
+test('SignUp Page test1', async({page}) => {
+
+    await page.goto('https://www.demoblaze.com/')
+    await page.pause()
+    await page.click('//a[text()="Sign up"]');
+    await page.getByRole('textbox', { name: 'Username:' }).fill('safaa995');
+    await page.getByRole('textbox', { name: 'Password:' }).fill('19941995');
+    await page.getByRole('button', { name: 'Sign up' }).click();
+
+})
